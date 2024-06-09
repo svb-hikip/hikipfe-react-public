011d305fc99b68772dc5aad658381fe25255af98
Kiruba <kirubakaran260202@gmail.com>
2024-06-09 19:19:12 +0530

    updated Appointments

--- src/pages/practice/Appointments.jsx
+++ src/pages/practice/Appointments.jsx
@@ -6,11 +6,13 @@ import interactionPlugin from '@fullcalendar/interaction';
 import { fetchPracticeAppointments } from './../../apis/PracticeAPIs';  // Adjust the import based on your project structure
 import { fetchUserAttributes } from 'aws-amplify/auth';
 import AppointmentDetail from './AppointmentDetail';
+import AddAppointmentForm from '../../components/appointments/AddAppointmentForm';
 
 const CalendarComponent = () => {
   const [events, setEvents] = useState([]);
   const [selectedAppointment, setSelectedAppointment] = useState(null);
   const [isAppointmentDetailOpen, SetIsAppointmentDetailOpen] = useState(false);
+  const [isFormOpen, setIsFormOpen] = useState(false);
   const calendarRef = useRef(null);
 
   const fetchAppointments = async (start, end) => {
@@ -54,8 +56,21 @@ const CalendarComponent = () => {
     setSelectedAppointment(null);
   }
 
+  const toggleForm = () => {
+    setIsFormOpen(prevState => !prevState);
+  }
+
+  const handleFormSubmit = (appointment) => {
+    console.log(appointment);
+    toggleForm();
+  }
+
   return (
     <div className="calendar-container">
+      <button onClick={toggleForm} className='mb-4 px-4 py-2 bg-blue-500 text-white rounded-md'>
+        {isFormOpen ? 'Close Form' : 'Add Appointment'}
+      </button>
+      {isFormOpen && <AddAppointmentForm onSubmit={handleFormSubmit} />}
       <FullCalendar
         ref={calendarRef}
         plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
