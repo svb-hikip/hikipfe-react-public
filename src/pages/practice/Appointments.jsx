ae9ed9a3885188ed81044cc3c5ee36167c4b5e90
sathyapraba S <sathyapraba48@gmail.com>
2024-06-10 16:13:04 +0530

    appoinment page

--- src/pages/practice/Appointments.jsx
+++ src/pages/practice/Appointments.jsx
@@ -5,9 +5,14 @@ import timeGridPlugin from '@fullcalendar/timegrid';
 import interactionPlugin from '@fullcalendar/interaction';
 import { fetchPracticeAppointments } from '../../apis/PracticeAPIs';  // Adjust the import based on your project structure
 import { fetchUserAttributes } from 'aws-amplify/auth';
+import AppointmentForm from '../../components/appointments/AppointmentForm';
+import AppointmentDetail from './AppointmentDetails';
 
 const CalendarComponent = () => {
   const [events, setEvents] = useState([]);
+  const [selectedAppointment, setSelectedAppointment] = useState(null);
+  const [isAppointmentDetailOpen, SetIsAppointmentDetailOpen] = useState(false);
+  const [isFormOpen, setIsFormOpen] = useState(false);
   const calendarRef = useRef(null);
 
   const fetchAppointments = async (start, end) => {
@@ -42,9 +47,35 @@ const CalendarComponent = () => {
   const handleDatesSet = (dateInfo) => {
     fetchAppointments(dateInfo.start, dateInfo.end);
   };
+  const handleEventClick = (info) => {
+    setSelectedAppointment(info.event);
+    SetIsAppointmentDetailOpen(true);
+  }
+
+  const closeDetail = () => {
+    SetIsAppointmentDetailOpen(false);
+    setSelectedAppointment(null);
+  }
+
+  const toggleForm = () => {
+    setIsFormOpen(prevState => !prevState);
+  }
+
+  const handleFormSubmit = (appointment) => {
+    console.log(appointment);
+    toggleForm();
+  }
+
 
   return (
     <div className="calendar-container">
+<div className="flex justify-end">
+<button onClick={toggleForm} className='mb-4 px-3 py-2 bg-indigo-600 text-white rounded-md'>
+        {isFormOpen ? 'Close Form' : '+ Add Appointment'}
+      </button>
+      </div>
+      {isFormOpen && <AppointmentForm onSubmit={handleFormSubmit} />}
+      {!isFormOpen && 
       <FullCalendar
         ref={calendarRef}
         plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
@@ -57,10 +88,12 @@ const CalendarComponent = () => {
         height="auto"
         events={events}
         datesSet={handleDatesSet}
-        eventClick={(info) => {
-          alert(`Clinician: ${info.event.extendedProps.clinician_name}\nClient: ${info.event.title}\nLocation: ${info.event.extendedProps.location_name}\nServices: ${info.event.extendedProps.services_names.join(', ')}\nItems: ${info.event.extendedProps.items_names.join(', ')}\nDuration: ${info.event.extendedProps.duration}`);
-        }}
+        // eventClick={(info) => {
+        //   alert(`Clinician: ${info.event.extendedProps.clinician_name}\nClient: ${info.event.title}\nLocation: ${info.event.extendedProps.location_name}\nServices: ${info.event.extendedProps.services_names.join(', ')}\nItems: ${info.event.extendedProps.items_names.join(', ')}\nDuration: ${info.event.extendedProps.duration}`);
+        // }}
+        eventClick={handleEventClick}
       />
+}
     </div>
   );
 };
