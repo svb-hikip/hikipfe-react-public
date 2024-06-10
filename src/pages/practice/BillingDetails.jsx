ca0e8593242471bff0b1db829d7e8b26be6f311a
Kiruba <kirubakaran260202@gmail.com>
2024-06-10 10:27:18 +0530

    updated BillingDetails

--- src/pages/practice/BillingDetails.jsx
+++ src/pages/practice/BillingDetails.jsx
@@ -107,58 +107,6 @@ export default function BillingDetail() {
   const [selected, setSelected] = useState(moods[5])
   return (
     <div>
-      {JSON.stringify(data, null, 2)}
-      <header className="absolute inset-x-0 top-0 z-50 flex h-16 border-b border-gray-900/10">
-        <div className="mx-auto flex w-full max-w-7xl items-center justify-between px-4 sm:px-6 lg:px-8">
-          <div className="flex flex-1 items-center gap-x-6">
-            <button type="button" className="-m-3 p-3 md:hidden" onClick={() => setMobileMenuOpen(true)}>
-              <span className="sr-only">Open main menu</span>
-              <Bars3Icon className="h-5 w-5 text-gray-900" aria-hidden="true" />
-            </button>
-            <img
-              className="h-8 w-auto"
-              src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-              alt="Your Company"
-            />
-          </div>
-          <div className="flex flex-1 items-center justify-end gap-x-8">
-            <button type="button" className="-m-2.5 p-2.5 text-gray-400 hover:text-gray-500">
-              <span className="sr-only">View notifications</span>
-              <BellIcon className="h-6 w-6" aria-hidden="true" />
-            </button>
-            <a href="#" className="-m-1.5 p-1.5">
-              <span className="sr-only">Your profile</span>
-              <img
-                className="h-8 w-8 rounded-full bg-gray-800"
-                src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                alt=""
-              />
-            </a>
-          </div>
-        </div>
-        <Dialog className="lg:hidden" open={mobileMenuOpen} onClose={setMobileMenuOpen}>
-          <div className="fixed inset-0 z-50" />
-          <DialogPanel className="fixed inset-y-0 left-0 z-50 w-full overflow-y-auto bg-white px-4 pb-6 sm:max-w-sm sm:px-6 sm:ring-1 sm:ring-gray-900/10">
-            <div className="-ml-0.5 flex h-16 items-center gap-x-6">
-              <button type="button" className="-m-2.5 p-2.5 text-gray-700" onClick={() => setMobileMenuOpen(false)}>
-                <span className="sr-only">Close menu</span>
-                <XMarkIconOutline className="h-6 w-6" aria-hidden="true" />
-              </button>
-              <div className="-ml-0.5">
-                <a href="#" className="-m-1.5 block p-1.5">
-                  <span className="sr-only">Your Company</span>
-                  <img
-                    className="h-8 w-auto"
-                    src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-                    alt=""
-                  />
-                </a>
-              </div>
-            </div>
-          </DialogPanel>
-        </Dialog>
-      </header>
-
       <main>
         <header className="relative isolate pt-16">
           <div className="absolute inset-0 -z-10 overflow-hidden" aria-hidden="true">
