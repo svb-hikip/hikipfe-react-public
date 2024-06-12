e11fd6d1306568eac130be7d703bd11be346cc8e
Kiruba <kirubakaran260202@gmail.com>
2024-06-12 18:12:11 +0530

    updated AppointmentCalendarDetail

--- src/components/appointments/AppointmentCalendarDetail.jsx
+++ src/components/appointments/AppointmentCalendarDetail.jsx
@@ -8,16 +8,13 @@ const AppointmentDetail = ({isOpen, onClose, appointment}) => {
       <div className="relative w-full max-w-md p-4 mx-auto bg-white rounded-lg shadow-lg">
         <div className="flex justify-between items-center pb-3">
           <h3 className="text-lg font-semibold">Appointment Details</h3>
-          {/* <button onClick={onClose} className="text-gray-500 hover:text-gray-700">
-            &times;
-          </button> */}
         </div>
         <div className="mt-2">
           <p><strong>Clinician:</strong> {appointment.clinician_name}</p>
           <p><strong>Client:</strong> {appointment.client_name}</p>
           <p><strong>Location:</strong> {appointment.location_name}</p>
-          <p><strong>Services:</strong> {appointment.services_names.join(', ')}</p>
-          <p><strong>Items:</strong> {appointment.items_names.join(', ')}</p>
+          <p><strong>Services:</strong> {appointment.services_names ? appointment.services_names.join(', '): 'N/A'}</p>
+          <p><strong>Items:</strong> {appointment.items_names ? appointment.items_names.join(', ') : 'N/A'}</p>
           <p><strong>Duration:</strong> {appointment.duration}</p>
         </div>
         <div className="flex justify-end mt-4">
