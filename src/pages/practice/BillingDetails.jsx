bd2ae4fbf1035d8b24b3fa5ae807cdf664994ce0
Aditya Sunil Wakale <adityawakale208@gmail.com>
2024-06-13 21:48:13 +0530

    Invoice issue date and due date added

--- src/pages/practice/BillingDetails.jsx
+++ src/pages/practice/BillingDetails.jsx
@@ -117,7 +117,8 @@ export default function BillingDetail() {
   const [selected, setSelected] = useState(moods[5])
   return (
     <div>
-<header className="relative isolate pt-16">
+      <main>
+        <header className="relative isolate pt-16">
           <div className="absolute inset-0 -z-10 overflow-hidden" aria-hidden="true">
             <div className="absolute left-16 top-full -mt-16 transform-gpu opacity-50 blur-3xl xl:left-1/2 xl:-ml-80">
               <div
@@ -141,7 +142,7 @@ export default function BillingDetail() {
                 />
                 <h1>
                   <div className="text-sm leading-6 text-gray-500">
-                    Invoice <span className="text-gray-700">{number}</span>
+                    Invoice <span className="text-gray-700">#00011</span>
                   </div>
                   <div className="mt-1 text-base font-semibold leading-6 text-gray-900">Tuple, Inc</div>
                 </h1>
@@ -209,6 +210,257 @@ export default function BillingDetail() {
           </div>
         </header>
 
+        <div className="mx-auto max-w-7xl px-4 py-16 sm:px-6 lg:px-8">
+          <div className="mx-auto grid max-w-2xl grid-cols-1 grid-rows-1 items-start gap-x-8 gap-y-8 lg:mx-0 lg:max-w-none lg:grid-cols-3">
+            {/* Invoice summary */}
+            <div className="lg:col-start-3 lg:row-end-1">
+              <h2 className="sr-only">Summary</h2>
+              <div className="rounded-lg bg-gray-50 shadow-sm ring-1 ring-gray-900/5">
+                <dl className="flex flex-wrap">
+                  <div className="flex-auto pl-6 pt-6">
+                    <dt className="text-sm font-semibold leading-6 text-gray-900">Amount</dt>
+                    <dd className="mt-1 text-base font-semibold leading-6 text-gray-900">$10,560.00</dd>
+                  </div>
+                  <div className="flex-none self-end px-6 pt-4">
+                    <dt className="sr-only">Status</dt>
+                    <dd className="rounded-md bg-green-50 px-2 py-1 text-xs font-medium text-green-600 ring-1 ring-inset ring-green-600/20">
+                      Paid
+                    </dd>
+                  </div>
+                  <div className="mt-6 flex w-full flex-none gap-x-4 border-t border-gray-900/5 px-6 pt-6">
+                    <dt className="flex-none">
+                      <span className="sr-only">Client</span>
+                      <UserCircleIcon className="h-6 w-5 text-gray-400" aria-hidden="true" />
+                    </dt>
+                    <dd className="text-sm font-medium leading-6 text-gray-900">Alex Curren</dd>
+                  </div>
+                  <div className="mt-4 flex w-full flex-none gap-x-4 px-6">
+                    <dt className="flex-none">
+                      <span className="sr-only">Due date</span>
+                      <CalendarDaysIcon className="h-6 w-5 text-gray-400" aria-hidden="true" />
+                    </dt>
+                    <dd className="text-sm leading-6 text-gray-500">
+                      <time dateTime="2023-01-31">January 31, 2023</time>
+                    </dd>
+                  </div>
+                  <div className="mt-4 flex w-full flex-none gap-x-4 px-6">
+                    <dt className="flex-none">
+                      <span className="sr-only">Status</span>
+                      <CreditCardIcon className="h-6 w-5 text-gray-400" aria-hidden="true" />
+                    </dt>
+                    <dd className="text-sm leading-6 text-gray-500">Paid with MasterCard</dd>
+                  </div>
+                </dl>
+                <div className="mt-6 border-t border-gray-900/5 px-6 py-6">
+                  <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
+                    Download receipt <span aria-hidden="true">&rarr;</span>
+                  </a>
+                </div>
+              </div>
+            </div>
+            {/* Invoice feild start from here..*/}
+            <div className="-mx-4 px-4 py-8 shadow-sm ring-1 ring-gray-900/5 sm:mx-0 sm:rounded-lg sm:px-8 sm:pb-14 lg:col-span-2 lg:row-span-2 lg:row-end-2 xl:px-16 xl:pb-20 xl:pt-16">
+              <h2 className="text-base font-semibold leading-6 text-gray-900">Invoice</h2>
+              <dl className="mt-6 grid grid-cols-1 text-sm leading-6 sm:grid-cols-2">
+                <div className="sm:pr-4">
+                  <dt className="inline text-gray-500">Issued on</dt>{' '}
+                  <dd className="inline text-gray-700">
+                    <time dateTime={date}>{new Date(date).toLocaleDateString()}</time>
+                  </dd>
+                </div>
+                <div className="mt-2 sm:mt-0 sm:pl-4">
+                  <dt className="inline text-gray-500">Due on</dt>{' '}
+                  <dd className="inline text-gray-700">
+                    {/* Calculated the due date field based on issued date */}
+                    <time dateTime={date}>{new Date(new Date(date).getTime() + 7 * 24 * 60 * 60 * 1000).toLocaleDateString()}</time>
+                  </dd>
+                </div>
+              </dl>
+             
+            </div>
+            {/*Invoce will end here. */}
+            <div className="lg:col-start-3">
+              {/* Activity feed */}
+              <h2 className="text-sm font-semibold leading-6 text-gray-900">Activity</h2>
+              <ul role="list" className="mt-6 space-y-6">
+                {activity.map((activityItem, activityItemIdx) => (
+                  <li key={activityItem.id} className="relative flex gap-x-4">
+                    <div
+                      className={classNames(
+                        activityItemIdx === activity.length - 1 ? 'h-6' : '-bottom-6',
+                        'absolute left-0 top-0 flex w-6 justify-center'
+                      )}
+                    >
+                      <div className="w-px bg-gray-200" />
+                    </div>
+                    {activityItem.type === 'commented' ? (
+                      <>
+                        <img
+                          src={activityItem.person.imageUrl}
+                          alt=""
+                          className="relative mt-3 h-6 w-6 flex-none rounded-full bg-gray-50"
+                        />
+                        <div className="flex-auto rounded-md p-3 ring-1 ring-inset ring-gray-200">
+                          <div className="flex justify-between gap-x-4">
+                            <div className="py-0.5 text-xs leading-5 text-gray-500">
+                              <span className="font-medium text-gray-900">{activityItem.person.name}</span> commented
+                            </div>
+                            <time
+                              dateTime={activityItem.dateTime}
+                              className="flex-none py-0.5 text-xs leading-5 text-gray-500"
+                            >
+                              {activityItem.date}
+                            </time>
+                          </div>
+                          <p className="text-sm leading-6 text-gray-500">{activityItem.comment}</p>
+                        </div>
+                      </>
+                    ) : (
+                      <>
+                        <div className="relative flex h-6 w-6 flex-none items-center justify-center bg-white">
+                          {activityItem.type === 'paid' ? (
+                            <CheckCircleIcon className="h-6 w-6 text-indigo-600" aria-hidden="true" />
+                          ) : (
+                            <div className="h-1.5 w-1.5 rounded-full bg-gray-100 ring-1 ring-gray-300" />
+                          )}
+                        </div>
+                        <p className="flex-auto py-0.5 text-xs leading-5 text-gray-500">
+                          <span className="font-medium text-gray-900">{activityItem.person.name}</span>{' '}
+                          {activityItem.type} the invoice.
+                        </p>
+                        <time
+                          dateTime={activityItem.dateTime}
+                          className="flex-none py-0.5 text-xs leading-5 text-gray-500"
+                        >
+                          {activityItem.date}
+                        </time>
+                      </>
+                    )}
+                  </li>
+                ))}
+              </ul>
+
+              {/* New comment form */}
+              <div className="mt-6 flex gap-x-3">
+                <img
+                  src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                  alt=""
+                  className="h-6 w-6 flex-none rounded-full bg-gray-50"
+                />
+                <form action="#" className="relative flex-auto">
+                  <div className="overflow-hidden rounded-lg pb-12 shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-indigo-600">
+                    <label htmlFor="comment" className="sr-only">
+                      Add your comment
+                    </label>
+                    <textarea
+                      rows={2}
+                      name="comment"
+                      id="comment"
+                      className="block w-full resize-none border-0 bg-transparent py-1.5 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+                      placeholder="Add your comment..."
+                      defaultValue={''}
+                    />
+                  </div>
+
+                  <div className="absolute inset-x-0 bottom-0 flex justify-between py-2 pl-3 pr-2">
+                    <div className="flex items-center space-x-5">
+                      <div className="flex items-center">
+                        <button
+                          type="button"
+                          className="-m-2.5 flex h-10 w-10 items-center justify-center rounded-full text-gray-400 hover:text-gray-500"
+                        >
+                          <PaperClipIcon className="h-5 w-5" aria-hidden="true" />
+                          <span className="sr-only">Attach a file</span>
+                        </button>
+                      </div>
+                      <div className="flex items-center">
+                        <Listbox value={selected} onChange={setSelected}>
+                          {({ open }) => (
+                            <>
+                              <Label className="sr-only">Your mood</Label>
+                              <div className="relative">
+                                <ListboxButton className="relative -m-2.5 flex h-10 w-10 items-center justify-center rounded-full text-gray-400 hover:text-gray-500">
+                                  <span className="flex items-center justify-center">
+                                    {selected.value === null ? (
+                                      <span>
+                                        <FaceSmileIcon className="h-5 w-5 flex-shrink-0" aria-hidden="true" />
+                                        <span className="sr-only">Add your mood</span>
+                                      </span>
+                                    ) : (
+                                      <span>
+                                        <span
+                                          className={classNames(
+                                            selected.bgColor,
+                                            'flex h-8 w-8 items-center justify-center rounded-full'
+                                          )}
+                                        >
+                                          <selected.icon
+                                            className="h-5 w-5 flex-shrink-0 text-white"
+                                            aria-hidden="true"
+                                          />
+                                        </span>
+                                        <span className="sr-only">{selected.name}</span>
+                                      </span>
+                                    )}
+                                  </span>
+                                </ListboxButton>
+
+                                <Transition
+                                  show={open}
+                                  leave="transition ease-in duration-100"
+                                  leaveFrom="opacity-100"
+                                  leaveTo="opacity-0"
+                                >
+                                  <ListboxOptions className="absolute z-10 -ml-6 mt-1 w-60 rounded-lg bg-white py-3 text-base shadow ring-1 ring-black ring-opacity-5 focus:outline-none sm:ml-auto sm:w-64 sm:text-sm">
+                                    {moods.map((mood) => (
+                                      <ListboxOption
+                                        key={mood.value}
+                                        className={({ focus }) =>
+                                          classNames(
+                                            focus ? 'bg-gray-100' : '',
+                                            !focus ? 'bg-white' : '',
+                                            'relative cursor-default select-none px-3 py-2'
+                                          )
+                                        }
+                                        value={mood}
+                                      >
+                                        <div className="flex items-center">
+                                          <div
+                                            className={classNames(
+                                              mood.bgColor,
+                                              'flex h-8 w-8 items-center justify-center rounded-full'
+                                            )}
+                                          >
+                                            <mood.icon
+                                              className={classNames(mood.iconColor, 'h-5 w-5 flex-shrink-0')}
+                                              aria-hidden="true"
+                                            />
+                                          </div>
+                                          <span className="ml-3 block truncate font-medium">{mood.name}</span>
+                                        </div>
+                                      </ListboxOption>
+                                    ))}
+                                  </ListboxOptions>
+                                </Transition>
+                              </div>
+                            </>
+                          )}
+                        </Listbox>
+                      </div>
+                    </div>
+                    <button
+                      type="submit"
+                      className="rounded-md bg-white px-2.5 py-1.5 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
+                    >
+                      Comment
+                    </button>
+                  </div>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+      </main>
     </div>
   )
 }
