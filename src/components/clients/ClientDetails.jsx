212a07eb9de3c44cdcf5c389d6422dfb8d3bb73c
svb0866 <svb0866@gmail.com>
2024-06-14 16:48:22 +0530

    Changed Client detail Page

--- src/components/clients/ClientDetails.jsx
+++ src/components/clients/ClientDetails.jsx
@@ -20,20 +20,71 @@ function ClientDetails() {
     <>
       <div key={clientData} className="p-6 bg-white shadow-lg rounded-lg mb-6">
         <div className="px-4 sm:px-0">
-          <h3 className="text-xl font-semibold leading-7 text-gray-900">Client Information</h3>
-          <p className="mt-1 max-w-2xl text-sm leading-6 text-gray-500">Personal details and Appointments</p>
+          <h3 className="text-xl font-semibold leading-7 text-gray-900">{clientData.contact.legal_first_name} {clientData.contact.legal_last_name}</h3>
+          <span className="ml-2 inline-block bg-blue-100 text-blue-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded">{clientData.client_type}</span>
+          <span className="ml-2 inline-block bg-green-100 text-green-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded">{clientData.billing_type}</span>
+          <span className="ml-2 text-xs font-medium mr-2 px-2.5 py-0.5 rounded">17/11/2023 (28 years)</span>
+          <p className="mt-1 max-w-2xl text-sm leading-6 text-gray-500">
+          <button
+            className="mt-4 inline-flex items-center text-indigo-600 hover:text-indigo-800 focus:outline-none focus:underline focus:ring-2 focus:ring-indigo-500 transition duration-150 ease-in-out"
+            onClick={() => setIsSideOverOpen(true)}
+          >
+            Edit Details
+          </button>
+
+
+          </p>
         </div>
-        <dl className="grid grid-cols-1 sm:grid-cols-2 gap-3">
-          <div className="mt-6">
-            <dl className="grid grid-cols-1 sm:grid-cols-1 gap-3">
-              <div className="border-t border-gray-200 px-4 py-3 sm:px-0 hover:bg-gray-50 rounded-lg">
-                <dt className="text-sm font-medium leading-6 text-gray-900">Full name</dt>
-                <dd className="mt-1 text-sm leading-6 text-gray-700 sm:mt-2">
-                  <span className="font-semibold">{clientData.contact.legal_first_name} {clientData.contact.legal_last_name}</span>
-                  <span className="ml-2 inline-block bg-blue-100 text-blue-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded">{clientData.client_type}</span>
-                  <span className="ml-2 inline-block bg-green-100 text-green-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded">{clientData.billing_type}</span>
-                </dd>
-              </div>
+      </div>
+      <div className="container mx-auto">
+      <div className="grid grid-cols-1 sm:grid-cols-12 gap-4">
+        <div className="sm:col-span-8 col-span-1">
+          {/* Content for the larger column (9 parts) */}
+          <div>
+        <div className="sm:hidden">
+          <label htmlFor="tabs" className="sr-only">
+            Select a tab
+          </label>
+          <select
+            id="tabs"
+            name="tabs"
+            className="block w-full rounded-md border-gray-300 focus:border-indigo-500 focus:ring-indigo-500"
+            defaultValue={tabs.find(tab => window.location.pathname.includes(tab.to))}
+            onChange={(e) => {
+              navigate(e.target.value); // This will trigger a route change in React Router
+            }}
+          >
+            {tabs.map((tab) => (
+              <option key={tab.name} value={tab.to}>
+                {tab.name}
+              </option>
+            ))}
+          </select>
+        </div>
+        <div className="hidden sm:block">
+          <nav className="isolate flex divide-x divide-gray-200 rounded-lg shadow" aria-label="Tabs">
+            {tabs.map((tab, tabIdx) => (
+              <NavLink
+                key={tab.name}
+                to={tab.to}
+                className={({ isActive }) => classNames(
+                  isActive ? 'text-indigo-700' : 'text-gray-400 hover:text-gray-700',
+                  tabIdx === 0 ? 'rounded-l-lg' : '',
+                  tabIdx === tabs.length - 1 ? 'rounded-r-lg' : '',
+                  'group relative min-w-0 flex-1 overflow-hidden bg-white py-4 px-4 text-center text-sm font-medium hover:bg-gray-50 focus:z-10'
+                )}
+              >
+                <span>{tab.name}</span>
+              </NavLink>
+              ))}
+            </nav>
+          </div>
+        </div>
+        <Outlet />
+        </div>
+        <div className="sm:col-span-4 col-span-1">
+          {/* Content for the smaller column (3 parts) */}
+          <dl className="grid grid-cols-1 sm:grid-cols-1 gap-3">
               <div className="border-t border-gray-200 px-4 py-3 sm:px-0 hover:bg-gray-50 rounded-lg">
                 <dt className="text-sm font-medium leading-6 text-gray-900">Client Portal Access</dt>
                 <dd className="mt-1 text-sm leading-6 text-gray-700 sm:mt-2">
@@ -80,14 +131,7 @@ function ClientDetails() {
                 </dd>
               </div>
             </dl>
-            <button
-              className="mt-4 inline-flex items-center px-4 py-2 border border-indigo-600 text-indigo-600 text-sm font-medium rounded-md hover:bg-indigo-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 transition duration-150 ease-in-out"
-              onClick={() => setIsSideOverOpen(true)}
-            >
-              Edit
-            </button>
-          </div>
-          <div className="mt-6">
+            <div className="mt-6">
             <div className="border-t border-gray-200 px-4 py-3 sm:px-0 hover:bg-gray-50 rounded-lg">
               <dt className="text-sm font-medium leading-6 text-gray-900">Adam Sandler | Relation: Son</dt>
               <dd className="mt-1 text-sm leading-6 text-gray-700 sm:mt-2">
@@ -163,51 +207,10 @@ function ClientDetails() {
               Add Contact
             </button>
           </div>
-        </dl>
-      </div>
-
-      <div>
-        <div className="sm:hidden">
-          <label htmlFor="tabs" className="sr-only">
-            Select a tab
-          </label>
-          <select
-            id="tabs"
-            name="tabs"
-            className="block w-full rounded-md border-gray-300 focus:border-indigo-500 focus:ring-indigo-500"
-            defaultValue={tabs.find(tab => window.location.pathname.includes(tab.to))}
-            onChange={(e) => {
-              navigate(e.target.value); // This will trigger a route change in React Router
-            }}
-          >
-            {tabs.map((tab) => (
-              <option key={tab.name} value={tab.to}>
-                {tab.name}
-              </option>
-            ))}
-          </select>
-        </div>
-        <div className="hidden sm:block">
-          <nav className="isolate flex divide-x divide-gray-200 rounded-lg shadow" aria-label="Tabs">
-            {tabs.map((tab, tabIdx) => (
-              <NavLink
-                key={tab.name}
-                to={tab.to}
-                className={({ isActive }) => classNames(
-                  isActive ? 'text-indigo-700' : 'text-gray-400 hover:text-gray-700',
-                  tabIdx === 0 ? 'rounded-l-lg' : '',
-                  tabIdx === tabs.length - 1 ? 'rounded-r-lg' : '',
-                  'group relative min-w-0 flex-1 overflow-hidden bg-white py-4 px-4 text-center text-sm font-medium hover:bg-gray-50 focus:z-10'
-                )}
-              >
-                <span>{tab.name}</span>
-              </NavLink>
-            ))}
-          </nav>
         </div>
       </div>
+    </div>
       <ClientForm isOpen={isSideOverOpen} setIsOpen={setIsSideOverOpen} clientData={clientData} updateClientData={setClientData} />
-      <Outlet />
     </>
   );
 }
